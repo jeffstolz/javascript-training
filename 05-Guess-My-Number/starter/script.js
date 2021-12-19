d28a5846933e4e4f76036b99ab316677fc3f7238
Jeffrey Stolz <jeffstolz@Jeffreys-MacBook-Pro.local>
2021-12-19 15:55:27 -0500

    implementing game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,11 +10,35 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(`.number`).textContent = secretNumber;
+
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess);
 
   if (!guess) {
-    document.querySelector(`.message`).textContent = `No number!`;
+    document.querySelector(`.message`).textContent = `No number :/`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `Correct number!!`;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Too high...`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(`.message`).textContent = `YOU LOSE :(`;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `Too low...`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.score`).textContent = 0;
+      document.querySelector(`.message`).textContent = `YOU LOSE :(`;
+    }
   }
 });
