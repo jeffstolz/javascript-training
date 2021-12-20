d4ba9e7bb308b92192a5a63cc4f22ee15ec2a764
Jeffrey Stolz <jeffstolz@Jeffreys-MacBook-Pro.local>
2021-12-19 20:07:07 -0500

    coding challenge 1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,9 +10,9 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(`.number`).textContent = secretNumber;
+console.log(secretNumber);
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -25,6 +25,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `Correct number!!`;
+    document.querySelector(`.number`).textContent = secretNumber;
 
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
@@ -51,4 +52,15 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.message`).textContent = `YOU LOSE :(`;
     }
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
 });
