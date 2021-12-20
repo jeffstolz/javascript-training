816e00895f7f126c95588aad1a59680ceaab8f13
Jeffrey Stolz <jeffstolz@Jeffreys-MacBook-Pro.local>
2021-12-20 12:07:20 -0500

    new project - modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// add an onclick event listener for each button
+// on click, show the modal, show the background blur
+
+// add an onclick event listener for the background blur and modal close button
+// on click, hide the modal, hide the background blur
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
+
+console.log(btnsOpenModal);
