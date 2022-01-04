df293d856768fe05acb20a6f4d0c1a9b027b0886
Jeffrey Stolz <jeffstolz@Jeffreys-MacBook-Pro.local>
2022-01-04 16:43:33 -0500

    Code challenge 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -65,3 +65,42 @@ printGoals(...game.scored);
 
 team1 < team2 && console.log(`Team 1 is more likely to win`);
 team2 < team1 && console.log(`Team 2 is more likely to win`);
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+gameEvents.delete(64);
+console.log(gameEvents.keys());
+
+const array = [...new Set(gameEvents.values())];
+console.log(array);
+
+let totalMinutes = 0;
+for (let value of gameEvents.keys()) {
+  totalMinutes += value;
+}
+console.log(
+  `The average minute an event happened this game was ${
+    totalMinutes / gameEvents.size
+  }`
+);
+
+for (const [key, value] of gameEvents.entries()) {
+  key < 45 && console.log(`[FIRST HALF] ${(key, value)}`);
+  key > 45 && console.log(`[SECOND HALF] ${(key, value)}`);
+}
